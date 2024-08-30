170901c684fa42733b45dca659c4cc8751fdf9fc
YamYam <31015878+YamilSalazar@users.noreply.github.com>
2024-08-29 20:16:09 -0600

    Add files via upload
    Correción en los poderes de Korby

--- Src/Korby.java
+++ Src/Korby.java
@@ -1,11 +1,19 @@
+/**
+ * La clase {@code Korby} es una subclase de {@code Personaje} que representa
+ * a un personaje específico llamado Korby. Este personaje tiene un nombre fijo,
+ * una vida inicial de 100 puntos, y tres ataques predeterminados: Ataque de Fuego,
+ * Ataque Torbellino y Cañon de Hielo.
+ */
 public class Korby extends Personaje {
 
-    public Korby() {
-        super("Korby", 100, new AtaqueFuego(), new AtaqueTorbellino(), new CanonDeHielo());
-    }
-
-    @Override
-    public String getNombre() {
-        return "Korby";
+    /**
+     * Constructor que crea una instancia de Korby con un nombre predeterminado
+     * de "Korby", 100 puntos de vida, y tres ataques predefinidos: Ataque de Fuego,
+     * Ataque Torbellino y Cañón de Hielo.
+     *
+     * @param mediador El mediador que gestiona las interacciones del combate entre los personajes.
+     */
+    public Korby(MediadorCombate mediador) {
+        super("Korby", 100, new TransformacionDragon(), new TransformacionEspada(), new TransformacionTanque(), mediador);
     }
 }
